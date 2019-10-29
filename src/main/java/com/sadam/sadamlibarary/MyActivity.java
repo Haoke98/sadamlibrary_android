7a47bfd6998cb088be9abb48a796de8d682d8b3b
Sadam <1903249375@qq.com>
2019-10-30 00:47:52 +0800

    data immigrator has been changed ,has been upgraded.

--- src/main/java/com/sadam/sadamlibarary/MyActivity.java
+++ src/main/java/com/sadam/sadamlibarary/MyActivity.java
@@ -22,8 +22,11 @@ import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
 import static com.sadam.sadamlibarary.Tools.isLightColor;
 
@@ -62,10 +65,10 @@ public abstract class MyActivity extends AppCompatActivity {
     public class DataImmigrator {
 
 
-        public int copyAndImmigrateDataToLitePal(String resourse_databasename, String resourse_tablename, int resourse_database_version, Class target_modelClass, String[] blackList, int r_raw_db_sqlite_id) {
+        public int copyAndImmigrateDataToLitePal(String resourse_databasename, String resourse_tablename, int resourse_database_version, Class target_modelClass, String[] sqlite_columns,String [] litepal_columns, int r_raw_db_sqlite_id) {
             int count = -1;
             if (copyDatabaseFileToTheDirectory(r_raw_db_sqlite_id)) {
-                count = immigrateDataToLitePal(resourse_databasename, resourse_tablename, resourse_database_version, target_modelClass, blackList);
+                count = immigrateDataToLitePal(resourse_databasename, resourse_tablename, resourse_database_version, target_modelClass, sqlite_columns,litepal_columns);
                 return count;
             } else {
                 return count;
@@ -79,12 +82,65 @@ public abstract class MyActivity extends AppCompatActivity {
          * @param resourse_tablename        普通数据库的 table 名
          * @param resourse_database_version 普通数据库的版本号
          * @param target_modelClass         LitePal数据库的映射模型类
-         * @param blackList                 LitePal数据库映射模型里一些非字段变量 以及 与普通Sqlite数据库里不对口的数据
+         * @param litepal_columns                 LitePal数据库映射模型里一些非字段变量 以及 与普通Sqlite数据库里不对口的数据
          * @return 返回成功转移的数据数量
+         *
+         *
+         * 就是先把   LitePal_model_columns = { name , age  ,  sex  }   -->   Fields =  {  Field(name),  Field(age)  , Field( sex ) }
+         *            sqlite_columns   = {  name   ,  年龄   , }
+         *
+         *            很容易发现 这里的    age 和  年龄  虽然指的是同一个事物，可是命名格式不一样 所以得   年龄 --> age
+         *
+         *            咱就指定那些 特定的 变量就ok    Lite_Pal_columns剩下的  按 原来的思路来： 把字段本生作为  原生 sqlite 里的 columns 取，
+         *
+         *            如果  不想给某些字段指定（比如  LitePal_model 里的某些变量 不是字段 而是  普通变量， 而且  这些变量在  原生sqlite里也没有
+         *
+         *            指定 两个列表
+         *
+         *                                  columnIndex_LitePal_model  =  { age ，sex }
+         *                                      columnIndex_resourse  = { 年龄 , null }
+         *
+         *            进入到函数内部就先遍历成
+         *
+         *            Key = { Field (name) , Field(age) , Field(sex) }
+         *          value = { name , 年龄  , null }
+         *
+         *
+         *          一般 都          Fields.length  >=  lite_pal_mode_column_index.length        因为我人比较懒   自己动手制定的肯定比类里定义的要少  有一种情况就是，你有可能给每个字段都制定了，这时候时两个一样大。
+         *
+         *            所以           for   i  in   lite_pal_mode_column_index :
+         *                                  Fields.
+         *
+         *                                  但是你还得给剩下的都会指定以下  名字  时  还得 遍历一遍
+         *                    所以  遍历的时候就判断一下  再 赋值
+         *
+         *                           for (Field field:fields){
+         *                              for  i   in  LitePal_column:
+         *                               if( field.getName().equals(i)){
+         *                                   Value[j] = sqlite_column_index[i.index]
+         *                               }else{
+         *                                   Vale[j]=field.getName()
+         *                               }
+         *                           }
+         *
+         *
+         *                           !idea:    利用  HashMap  类
+         *
+         *                           先把    所有  Field  以 它们的   name  作为  它们的  key  添加到里面
+         *
+         *                                          HashMap.put(Field.getName(),Field.getName())
+         *
+         *                           再     for  i in  range  litepal_column.length:
+         *
+         *                                      HashMap.put( litePal_column[i] , sqlite_columns[i] );
+         *
+         *
+         *                            为了避免  重复劳动   先  判断一下     fields.length ==  lite_pal_columns.length
          */
-        protected int immigrateDataToLitePal(String resourse_databasename, String resourse_tablename, int resourse_database_version, Class target_modelClass, String[] blackList) {
+        protected int immigrateDataToLitePal(String resourse_databasename, String resourse_tablename, int resourse_database_version, Class target_modelClass, String[] sqlite_columns,String [] litepal_columns) {
             MyDatabaseHelper myDatabaseHelper = new MyDatabaseHelper(MyActivity.this, resourse_databasename, null, resourse_database_version);
             SQLiteDatabase sqLiteDatabase = myDatabaseHelper.getReadableDatabase();
+
             Cursor cursor = sqLiteDatabase.query(resourse_tablename, null, null, null, null, null, null, null);
             int lastcount = DataSupport.count(target_modelClass);
             Log.e(TAG, "原有的数据量：" + lastcount);
@@ -95,47 +151,42 @@ public abstract class MyActivity extends AppCompatActivity {
             }
             Log.e(TAG, "开始迁移数据...........");
             Field[] fields = target_modelClass.getDeclaredFields();
-            LinkedList<Field> linkedList = new LinkedList<>(Arrays.asList(fields));
-            LinkedList<Field> willbedelet = new LinkedList<>();
-            Log.e(TAG, fields.length + "  " + linkedList.size());
-            for (Field field : linkedList) {
-                for (String name : blackList) {
-                    Log.e(TAG, field.getName() + "   " + name);
-                    if (name.equals(field.getName())) {
-                        willbedelet.add(field);
-                        Log.e(TAG, "removed");
-                    }
+            HashMap<String, String> hashMap = new HashMap<>();
+            if (fields.length == litepal_columns.length) {
+                for (int i = 0; i < litepal_columns.length; i++) {
+                    hashMap.put(litepal_columns[i], sqlite_columns[i]);
+                }
+            } else {
+                for (Field field : fields) {
+                    hashMap.put(field.getName(), field.getName());
+                }
+                for (int i = 0; i < litepal_columns.length; i++) {
+                    hashMap.put(litepal_columns[i], sqlite_columns[i]);
                 }
             }
-            Log.e(TAG, fields.length + "  " + linkedList.size());
-            for (Field field : willbedelet) {
-                linkedList.remove(field);
-            }
-            Log.e(TAG, fields.length + "  " + linkedList.size());
-
             if (cursor.moveToFirst()) {
                 Log.e(TAG, "MoveToFirst");
                 Log.e(TAG, "FirstWord");
                 do {
                     try {
                         DataSupport object = (DataSupport) target_modelClass.newInstance();
-                        for (Field field : linkedList) {
-                            if (field.getName() == "id") continue;
-                            else {
-                                String methodName = "set" + field.getName();
-                                Method method = target_modelClass.getDeclaredMethod(methodName, field.getType());
-                                Class<?> type = field.getType();
-                                int columnIndex = cursor.getColumnIndex(field.getName());
+                        for (Field field : fields) {
+                            String sqlite_columns_name = hashMap.get(field.getName());
+                            if (hashMap.get(field.getName()) != null) {
+                                Method method = target_modelClass.getDeclaredMethod("set" + field.getName(), field.getType());
+                                Class<?> dataType = field.getType();
+                                int columnIndex = cursor.getColumnIndex(sqlite_columns_name);
                                 Object data = null;
-                                if (type == String.class) {
+                                if (dataType == String.class) {
                                     data = cursor.getString(columnIndex);
-                                } else if (type == int.class) {
+                                } else if (dataType == int.class) {
                                     data = cursor.getInt(columnIndex);
                                 } else {
                                     data = cursor.getBlob(columnIndex);
                                 }
 //                                Log.e(TAG,data.toString());
                                 method.invoke(object, data);
+                            } else {
                             }
                         }
                         object.save();
@@ -143,9 +194,9 @@ public abstract class MyActivity extends AppCompatActivity {
                         e.printStackTrace();
                     } catch (InstantiationException e) {
                         e.printStackTrace();
-                    } catch (NoSuchMethodException e) {
-                        e.printStackTrace();
                     } catch (InvocationTargetException e) {
+                        e.printStackTrace();
+                    } catch (NoSuchMethodException e) {
                         e.printStackTrace();
                     }
                 } while (cursor.moveToNext());
@@ -154,12 +205,6 @@ public abstract class MyActivity extends AppCompatActivity {
             Log.e(TAG, "总共迁移数据量：" + count);
             return count;
         }
-
-//        test5 taskmanger;
-//        dfsfjkdsjl
-//        mfsdm,fm.ds/
-//        git test seven
-
 
         /**
          * 复制数据库文件 从res.raw目录 --> 到App缓存目录的databases目录
